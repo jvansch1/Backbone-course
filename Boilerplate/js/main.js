55323e6ebbeb9e05af35e0b14232f878ca2e43bc
John Van Schultz <jvansch1@binghamton.edu>
2017-07-05 14:52:16 -0400

    First Project

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,26 +1,33 @@
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
-
-const Animal = Backbone.Model.extend({
-  walk: function() {
-    console.log("Animal is walking...");
-  }
-})
-
-const Dog = Animal.extend({
-  bark: function() {
-    console.log("Barking!")
+const Vehicle = Backbone.Model.extend({
+  idAttribute: "registrationNumber",
+  validate: function(attrs) {
+    if (!attrs.registrationNumber) {
+      return "Must have Registration Number"
+    }
   },
-  walk: function() {
-    console.log("Dog is walking...")
+  urlRoot: "/api/vehicles",
+  start: function() {
+    console.log("Vehicle started")
   }
 });
 
-const dog = new Dog();
+const Car = Vehicle.extend({
+  start: function() {
+    console.log(`Car with number ${this.get("registrationNumber")} started`)
+  }
+});
 
-const animal = new Animal();
+const car = new Car({registrationNumber: "XLI887", color: "Blue"})
 
-dog.walk();
-dog.bark();
+if (!car.isValid()) {
+  console.log(car.validationError)
+}
+
+// car.set("registrationNumber", "XLI887")
+
+if (!car.isValid()) {
+  console.log(car.validationError)
+}
+
+car.start();
