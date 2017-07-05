984a4dee5b3356828d55e45f4381258777f298bc
John Van Schultz <jvansch1@binghamton.edu>
2017-07-05 15:03:46 -0400

    Add first collection

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,33 +1,43 @@
+//
+// const Vehicle = Backbone.Model.extend({
+//   idAttribute: "registrationNumber",
+//   validate: function(attrs) {
+//     if (!attrs.registrationNumber) {
+//       return "Must have Registration Number"
+//     }
+//   },
+//   urlRoot: "/api/vehicles",
+//   start: function() {
+//     console.log("Vehicle started")
+//   }
+// });
+//
+// const Car = Vehicle.extend({
+//   start: function() {
+//     console.log(`Car with number ${this.get("registrationNumber")} started`)
+//   }
+// });
+//
+// const car = new Car({registrationNumber: "XLI887", color: "Blue"})
+//
+// if (!car.isValid()) {
+//   console.log(car.validationError)
+// }
+//
+// // car.set("registrationNumber", "XLI887")
+//
+// if (!car.isValid()) {
+//   console.log(car.validationError)
+// }
+//
+// car.start();
 
-const Vehicle = Backbone.Model.extend({
-  idAttribute: "registrationNumber",
-  validate: function(attrs) {
-    if (!attrs.registrationNumber) {
-      return "Must have Registration Number"
-    }
-  },
-  urlRoot: "/api/vehicles",
-  start: function() {
-    console.log("Vehicle started")
-  }
-});
+const Song = Backbone.Model.extend();
 
-const Car = Vehicle.extend({
-  start: function() {
-    console.log(`Car with number ${this.get("registrationNumber")} started`)
-  }
-});
+const Songs = Backbone.Collection.extend({
+  model: Song
+})
 
-const car = new Car({registrationNumber: "XLI887", color: "Blue"})
+const songs = new Songs();
 
-if (!car.isValid()) {
-  console.log(car.validationError)
-}
-
-// car.set("registrationNumber", "XLI887")
-
-if (!car.isValid()) {
-  console.log(car.validationError)
-}
-
-car.start();
+songs.add(new Song({title: "One"}));
