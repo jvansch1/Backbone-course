24e67e6108161928be90807c53a84bbb443bc41a
John Van Schultz <jvansch1@binghamton.edu>
2017-07-06 17:48:04 -0400

    Router project

--- Routers/js/main.js
+++ Routers/js/main.js
@@ -3,24 +3,79 @@
 // Later, you'll see how to organize your code into separate
 // files and modules.
 
-let ArtistsView = Backbone.View.extend({
+// let ArtistsView = Backbone.View.extend({
+//   render: function() {
+//     this.$el.html("Artists!!!");
+//     return this;
+//   }
+// })
+//
+// let AlbumsView = Backbone.View.extend({
+//   render: function() {
+//     this.$el.html("Albums!!!");
+//     return this;
+//   }
+// })
+//
+// let GenresView = Backbone.View.extend({
+//   render: function() {
+//     this.$el.html("Genres!!!");
+//     return this;
+//   }
+// })
+//
+// let NavView = Backbone.View.extend({
+//   events: {
+//     "click": "onClick"
+//   },
+//   onClick: function(e) {
+//     let $li = $(e.target);
+//     router.navigate($li.attr('data-url'), { trigger: true });
+//   }
+// })
+//
+// let Router = Backbone.Router.extend({
+//   routes: {
+//     "albums": "viewAlbums",
+//     "albums/:albumId": "viewAlbumById",
+//     "artists": "viewArtists",
+//     "genres": "viewGenres",
+//     "*other": "defaultRoute"
+//   },
+//
+//   viewAlbums: function() {
+//     let view = new AlbumsView({ el: "#container" })
+//     view.render();
+//   },
+//
+//   viewArtists: function() {
+//     let view = new ArtistsView({ el: "#container" })
+//     view.render();
+//   },
+//   viewGenres: function() {
+//     let view = new GenresView({ el: "#container" })
+//     view.render();
+//   },
+//   defaultRoute: function() {
+//     console.log("default")
+//   }
+// })
+//
+// let navView = new NavView({ el: "#nav" })
+// let router = new Router();
+// Backbone.history.start();
+
+
+
+let CarsView = Backbone.View.extend({
   render: function() {
-    this.$el.html("Artists!!!");
-    return this;
+    this.$el.html("CARS!!!")
   }
 })
 
-let AlbumsView = Backbone.View.extend({
+let BoatsView = Backbone.View.extend({
   render: function() {
-    this.$el.html("Albums!!!");
-    return this;
-  }
-})
-
-let GenresView = Backbone.View.extend({
-  render: function() {
-    this.$el.html("Genres!!!");
-    return this;
+    this.$el.html("BOATS!!!")
   }
 })
 
@@ -29,38 +84,27 @@ let NavView = Backbone.View.extend({
     "click": "onClick"
   },
   onClick: function(e) {
-    let $li = $(e.target);
-    router.navigate($li.attr('data-url'), { trigger: true });
+    let target = $(e.target)
+    router.navigate(target.attr('data-url'), { trigger: true })
   }
 })
 
 let Router = Backbone.Router.extend({
   routes: {
-    "albums": "viewAlbums",
-    "albums/:albumId": "viewAlbumById",
-    "artists": "viewArtists",
-    "genres": "viewGenres",
-    "*other": "defaultRoute"
+    "cars": "viewCars",
+    "boats": "viewBoats"
   },
-
-  viewAlbums: function() {
-    let view = new AlbumsView({ el: "#container" })
+  viewCars: function() {
+    let view = new CarsView({ el: "#container" })
     view.render();
   },
-
-  viewArtists: function() {
-    let view = new ArtistsView({ el: "#container" })
+  viewBoats: function() {
+    let view = new BoatsView({ el: "#container" })
     view.render();
-  },
-  viewGenres: function() {
-    let view = new GenresView({ el: "#container" })
-    view.render();
-  },
-  defaultRoute: function() {
-    console.log("default")
   }
 })
 
-let navView = new NavView({ el: "#nav" })
 let router = new Router();
 Backbone.history.start();
+
+let navView = new NavView({ el: "#nav" });
