60fc9036604cccb181172e063f61b08fa980c471
John Van Schultz <jvansch1@binghamton.edu>
2017-07-06 16:54:25 -0400

    Finish events project

--- View_Project/js/main.js
+++ View_Project/js/main.js
@@ -20,17 +20,46 @@ const VehicleView = Backbone.View.extend({
   }
 });
 
+let bus = _.extend({}, Backbone.Events)
+
+const NewVehicleView = Backbone.View.extend({
+  initialize: function(options) {
+    this.bus = options.bus
+  },
+  events: {
+    "click #add-button": "addCar"
+  },
+  addCar: function() {
+    let id = $("#text-field")[0].value;
+    let newVehicle = new Vehicle({ registrationNumber: id, color: 'Grey'})
+    this.bus.trigger("addCar", newVehicle)
+  },
+  render: function() {
+    this.$el.html("<input type='text' id='text-field'></input><button id='add-button'>Add</button>")
+  }
+})
+
 const VehiclesView = Backbone.View.extend({
   tagName: 'ul',
   events: {
     "click .delete-button": "removeVehicle"
+  },
+  initialize: function(options) {
+    this.bus = options.bus
+    this.bus.on("addCar", this.addCar, this)
+  },
+  addCar: function(e) {
+    console.log(e)
+    let newVehicleView = new VehicleView({ model: e, registrationNumber: e.attributes.registrationNumber, color: e.attributes.color })
+    console.log(newVehicleView)
+    this.$el.append(newVehicleView.render().$el)
   },
   removeVehicle: function(e) {
     $(e.currentTarget).parent().remove();
   },
   render: function() {
     this.model.each(function(vehicle) {
-      let vehicleView = new VehicleView({ model: vehicle })
+      let vehicleView = new VehicleView({ model: vehicle, bus: self.bus})
       this.$el.append(vehicleView.render().$el)
     }.bind(this))
   }
@@ -41,6 +70,7 @@ let vehicles = new Vehicles([
   new Vehicle({ color: 'grey', registrationNumber: 'jsdf123', id: 2}),
   new Vehicle({ color: 'green', registrationNumber: 'x8h3jl', id: 3}),
 ])
-
-let vehiclesView = new VehiclesView({el: "#vehicles", model: vehicles})
+let newVehicleView = new NewVehicleView({ el: "#entry", bus: bus })
+newVehicleView.render();
+let vehiclesView = new VehiclesView({ el: "#vehicles", model: vehicles, bus: bus })
 vehiclesView.render();
